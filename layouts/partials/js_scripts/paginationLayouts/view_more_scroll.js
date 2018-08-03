346b943f93e640d260bb6b0dde5982c55e5783f4
Rahul Madhavan <rahul.maddy@gmail.com>
2018-08-03 08:19:19 +0530

    self implementing view more scroll

--- layouts/partials/js_scripts/paginationLayouts/view_more_scroll.js
+++ layouts/partials/js_scripts/paginationLayouts/view_more_scroll.js
@@ -1,13 +1,13 @@
 
 var element_container = document.querySelector('#{{($.Scratch.Get "layout_data").name}}_cards');
 // var append_elements = document.querySelector('.details_cards');
-var view_more_button = document.querySelector('.view-more-button');
+var view_more_button = document.querySelector('.view_more_button');
 // var next_page_url = document.querySelector('.pagination_next').href;
-var view_more_button_container = document.querySelector('.view-more-button-container');
-var loading_dots = document.querySelector('.loader-ellips');
-var status = document.querySelector('.page-load-status');
+var view_more_button_container = document.querySelector('.view_more_button_container');
+var loading_dots_container = document.querySelector('.loading_dots_container');
+var error_message = document.querySelector('.view_more_error');
 
-var total_pages = {{$.Paginator.TotalPages}};
+var total_pages = {{$.Paginator.TotalPages}} -1; //we subtract 1 so that it is in the format of 0,1,2...
 var current_page_num = 0;
 
 var base_url = "{{.URL}}";
@@ -18,6 +18,8 @@ var pages = [];
 console.log(pages);
 
 var jquery_request = function() {
+    $(loading_dots_container).css("display","block");
+    $(view_more_button_container).css("display","none");
     $.ajax({
         url: pages[current_page_num],
         context: document.body,
@@ -25,13 +27,34 @@ var jquery_request = function() {
             $(response).find('.details_cards').each(function( index ) {
                 $(element_container).append(this);
             });
+            $(loading_dots_container).css("display","none");
+            if( (total_pages-1) > current_page_num) {
+                $(view_more_button_container).css("display","block");
+            }
             lazyLoadFunction();
             // Use below if you want to change the URL of the page
             // history.pushState(null, null, pages[current_page_num]);
             current_page_num++;
-            if(current_page_num>=(total_pages-1)){
-                $(view_more_button_container).css("display","none");
+
+        },
+        error: function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Could not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
             }
+            $(error_message).innerHTML(msg);
         }
     });
 };
