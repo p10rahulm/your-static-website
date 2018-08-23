84fb88f5532dc5297ae2ebf90c67b874c5a5d701
Rahul Madhavan <rahul.maddy@gmail.com>
2018-08-23 23:49:19 +0530

    photoswipe plugin completed

--- layouts/partials/js_scripts/singlePageLayouts/books.js
+++ layouts/partials/js_scripts/singlePageLayouts/books.js
@@ -1,15 +1,15 @@
-var initialize_popup = function(index_clicked){
+var initialize_popup = function(index_clicked,imageHeights,imageWidths){
     var pswpElement = document.querySelectorAll('.pswp')[0];
     // build items array
+
     var items = [
-        {{range .Params.images}}
+        {{ range $index, $element := .Params.images }}
         {
-            src: '/img/{{.}}',
-            w: 900,
-            h: 900
+            src: '/img/{{$element}}',
+            w: imageWidths[{{$index}}],
+            h: imageHeights[{{$index}}]
         },
         {{end}}
-
     ];
     // define options (if needed)
     var options = {
@@ -36,31 +36,29 @@ var initialize_popup = function(index_clicked){
 
 document.addEventListener('DOMContentLoaded', function(){
     var index_clicked = 0;
+    var image_heights = [];
+    var image_widths = [];
     var otherimages = document.getElementsByClassName("other_images");
     var featured_image = document.getElementById("featured_image");
 
     for (var i = 0; otherimages.length>i; i++) {
         otherimages[i].addEventListener('click', image_array_clicked, false);
-        otherimages[i].image_index = i //can also be written as otherimages[i].["image_index"]
+        otherimages[i].image_index = i; //can also be written as otherimages[i].["image_index"]
+        // console.log("i = " + i + "otherimages[i].naturalheight ="+otherimages[i].naturalHeight );
+        image_heights.push(otherimages[i].naturalHeight);
+        image_widths.push(otherimages[i].naturalWidth);
     }
+    // console.log("image_heights = "+image_heights + "image_widths = "+image_widths);
 
     function image_array_clicked(evnt){
         // console.log("Clicked other images");
         if (evnt.target.tagName == 'IMG') { //This if is to be doubly sure an image was clicked
             var imageLink = evnt.target.getAttribute("src");
-            console.log(imageLink);
+            // console.log(imageLink);
             featured_image.setAttribute("src",imageLink);
         }
-        /*
-        for (var i = 0; otherimages.length>i; i++) {
-            if(otherimages[i]==evnt.target){
-                index_clicked = i;
-                break;
-            }
-        };
-        console.log("index_clicked = "+index_clicked);
-        */
-        console.log("evnt.target.image_index = "+evnt.target.image_index);
+        // for (var i = 0; otherimages.length>i; i++) {if(otherimages[i]==evnt.target){index_clicked = i;break;}};
+        // console.log("evnt.target.image_index = "+evnt.target.image_index);
         index_clicked = evnt.target.image_index;
     }
 
@@ -68,11 +66,9 @@ document.addEventListener('DOMContentLoaded', function(){
     featured_image.addEventListener("click", featured_image_clicked);
 
     function featured_image_clicked() {
-        console.log("featured_image_clicked");
-        console.log("index_clicked = "+index_clicked);
-        // gallery.options.index = index_clicked;
-        initialize_popup(index_clicked);
-        // gallery.init();
+        // console.log("featured_image_clicked");
+        // console.log("index_clicked = "+index_clicked);
+        initialize_popup(index_clicked,image_heights,image_widths);
     }
 
 }, false);
