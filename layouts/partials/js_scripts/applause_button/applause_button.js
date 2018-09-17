3fa1ee1380193b82b07a6c290b2afe34233023eb
Rahul Madhavan <rahul.maddy@gmail.com>
2018-09-18 03:20:50 +0530

    applause button formatting, js

---
+++ layouts/partials/js_scripts/applause_button/applause_button.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var applause_button = document.getElementById("applause_button");
+    if(window.innerWidth > 1200){
+        var container_width = document.getElementsByClassName("container")[0].offsetWidth;
+        var applause_left = Math.max(20,(window.innerWidth/2 - (container_width/2) -70));
+        applause_button.style.left = applause_left + "px";
+        applause_button.style.top = "100px";
+        applause_button.style.width = "55px";
+        applause_button.style.height = "55px";
+    } else {
+        applause_button.style.bottom = "38px";
+        applause_button.style.left = "5px";
+        applause_button.style.width = "45px";
+        applause_button.style.height = "45px";
+        applause_button.style.marginBottom = "0";
+    }
+
+}, false);
